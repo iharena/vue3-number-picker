f0cf12a3e67702fb6742b1ce0f09eccfdd9cf3e3
mgcodeur <mgcodeur@gmail.com>
2025-05-29 15:41:56 +0300

    ci: Tests PR
    Run Tests PR when push main

--- scripts/build.sh
+++ scripts/build.sh
@@ -52,4 +52,8 @@ npm run build:themes
 echo "🔨 Building the Vue library..."
 npm run build
 
+echo "🚀 Publishing to npm..."
+npm publish --access public --//registry.npmjs.org/:_authToken="$NODE_AUTH_TOKEN"
+echo "✅ Publish successful!"
+
 echo "✅ Publish successful!"
